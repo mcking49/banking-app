f051d90f40ffe4d0d24534060708010aaad508e5
Miten Chauhan <miten@chauhan.nz>
2023-02-28 10:10:40 +1100

    Add loading ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { SpinnerOverlay } from '@components/loading'
 
 import { useMeQuery } from './app/api'
 import Accounts from './pages/Accounts'
@@ -9,27 +10,35 @@ import ProtectedRoute from './pages/ProtectedRoute'
 import Transactions from './pages/Transactions'
 
 const App: FC = () => {
-  const { data: user } = useMeQuery(null)
+  const { data: user, isLoading } = useMeQuery(null)
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/dashboard/accounts" />} />
+    <>
+      {!isLoading && (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/dashboard/accounts" />} />
 
-        {/* User must be logged IN to access routes */}
-        <Route element={<ProtectedRoute isAllowed={!!user} redirectPath="/login" />}>
-          <Route path="/dashboard" element={<Dashboard />}>
-            <Route path="accounts" element={<Accounts />} />
-            <Route path="transactions" element={<Transactions />} />
-          </Route>
-        </Route>
+            {/* User must be logged IN to access routes */}
+            <Route element={<ProtectedRoute isAllowed={!!user} redirectPath="/login" />}>
+              <Route path="/dashboard" element={<Dashboard />}>
+                <Route path="accounts" element={<Accounts />} />
+                <Route path="transactions" element={<Transactions />} />
+              </Route>
+            </Route>
 
-        {/* User must be logged OUT to access routes */}
-        <Route element={<ProtectedRoute isAllowed={!user} redirectPath="/dashboard/accounts" />}>
-          <Route path="/login" element={<Login />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            {/* User must be logged OUT to access routes */}
+            <Route
+              element={<ProtectedRoute isAllowed={!user} redirectPath="/dashboard/accounts" />}
+            >
+              <Route path="/login" element={<Login />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      )}
+
+      <SpinnerOverlay isOpen={isLoading} />
+    </>
   )
 }
 
---
+++ src/components/loading/Spinner.tsx
@@ -0,0 +1,53 @@
+import { type FC, Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+type Props = {
+  isOpen: boolean
+}
+
+export const SpinnerOverlay: FC<Props> = ({ isOpen }) => {
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={() => {}}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="flex h-[200px] w-[200px] transform items-center justify-center overflow-hidden rounded-2xl bg-stone-900 p-6 shadow-xl transition-all">
+                <div className="flex items-center justify-center">
+                  <div
+                    className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                    role="status"
+                  >
+                    <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                      Loading...
+                    </span>
+                  </div>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
